b7fb2a7d091cae3837a54f935e4c00d6af048e5d
Rohit <rohitkumar@caplineservices.com>
2026-03-30 20:19:23 +0530

    add

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 console.log('Server is starting...');
 console
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! Welcome to the DevOps backend server.');
 });
 console.log('Basic route set up...');
 app.post('/add', (req, res) => {
